193209e8a98c9becbab3307b3f17c9f14f55df66
Nyyir Soobrattee <nyyirs@gmail.com>
2024-02-20 23:00:16 +0400

    Init byou.py => In Progress

--- src/modules/byou.py
+++ src/modules/byou.py
@@ -21,5 +21,32 @@ script_tag = soup.find('script', id='__NEXT_DATA__')
 json_str = script_tag.string if script_tag else '{}'
 data = json.loads(json_str)
 
-#[Catalogue] => Contains other forfaits
-#[offers] => main forfaits
+other_offers = data['props']['pageProps']['productsList']['catalogue']
+
+main_offers = data['props']['pageProps']['productsList']['offers']
+
+extracted_data = []
+
+for offer in main_offers:
+    data_envelope = offer['data_envelope']
+    newprice = offer['newprice']
+    # Determine if option5g is present and has data
+    option5g_present = bool(offer['option5g'])
+
+    # Always add the base entry with the actual 5G status
+    if option5g_present:
+        # If 5G data is present, calculate the adjusted price with the 5G option price
+        adjusted_price = newprice + offer['option5g']['price']
+        extracted_data.append([data_envelope, True, adjusted_price])
+    else:
+        extracted_data.append([data_envelope, False, newprice])
+
+    # Add a second entry with 5G set to False only if the 5G option was originally true
+    if option5g_present:
+        # For the second entry, use the original newprice without the 5G price addition
+        extracted_data.append([data_envelope, False, newprice])
+
+# Print the extracted data to verify the output
+for data in extracted_data:
+    print(data)
+
