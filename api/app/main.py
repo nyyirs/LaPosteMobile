a1c6a0c316594891a629ff340e6de374a8b86d2a
Nyyir Soobrattee <nyyirs@gmail.com>
2024-03-03 17:37:05 +0400

    remove /upload route and implement logic in /run

--- api/app/main.py
+++ api/app/main.py
@@ -1,4 +1,3 @@
-# Import FastAPI
 from fastapi import FastAPI
 import logging
 from modules.logging_config import configure_logging
@@ -23,42 +22,33 @@ from modules.syma import Syma
 from modules.youprice import Youprice
 from modules.azure_blob_uploader import AzureBlobUploader
 
-# Create an instance of the FastAPI class
 app = FastAPI()
-
-# Configure logging and get the custom error counter handler
-error_counter_handler, file_handler = configure_logging()
 
 # Your application code here
 logger = logging.getLogger(__name__)
 
-# Define a root `/` endpoint
 @app.get("/")
 async def root():
     return {"message": "Welcome to La Poste Mobile Scraping Tool"}
 
 @app.get("/run")
 async def run():
-    scrapers = [Auchan(), Byou(), Coriolis(), Creditmutuel(), Free(), Cdiscount(), Lapostemobile(),
-        Lebara(), Lyca(), Nrj(), Orange(), Prixtel(), Reglo(), Sfr(), Red(),
-        Sosh(), Syma(), Youprice()]    
+    # Configure logging and get the custom error counter handler
+    # This ensures logging starts here
+    error_counter_handler, file_handler = configure_logging()
+
+    scrapers = [Auchan()]
     for scraper in scrapers:
         logging.info("------------------------Start-----------------------")
         scraper.run()
         logging.info("------------------------End-------------------------\n")    
     logging.info(f"Total Errors Logged: {error_counter_handler.error_count} \n")
-    return {"message": "Scraping completed", "errors": error_counter_handler.error_count}
 
-@app.get("/upload")
-async def upload():
-    global file_handler  # Assuming file_handler is made global for access here
     uploader = AzureBlobUploader()
     # Close the file handler to release the log file before deleting it
     if file_handler:
         logging.getLogger().removeHandler(file_handler)
         file_handler.close()
     uploader.upload_log_file()  
-    return {"message": "Upload completed"}
 
-
-    
+    return {"message": "Scraping completed", "errors": error_counter_handler.error_count}
